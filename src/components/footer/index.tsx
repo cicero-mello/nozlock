4e4c68654d6c97ea2b597b45f8f45724681eb558
Cícero Mello <ciceromello.dev@gmail.com>
2025-07-19 23:29:19 -0300

    ✨ Increasing Footer UX

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,9 +1,15 @@
-import { Component } from "solid-js"
 import { GitHubSVG } from "@components/svg/git-hub"
 import styles from "./styles.module.css"
+import { Component } from "solid-js"
 import { A } from "@solidjs/router"
 
 export const Footer: Component = (props) => {
+
+    const handleAnchorClick = (
+        event: MouseEvent & { currentTarget: HTMLAnchorElement }
+    ) => {
+        event.currentTarget?.blur()
+    }
 
     return (
         <footer class={styles.footer} {...props}>
@@ -12,27 +18,38 @@ export const Footer: Component = (props) => {
                 target="_blank"
                 href="https://github.com/cicero-mello"
                 class={styles["svg-anchor"]}
+                onClick={handleAnchorClick}
             />
             <nav class={styles.nav}>
-                <A
-                    children="Import Vault"
-                    class={styles.anchor}
-                    href="/import-vault"
-                /> |
-                <A
-                    children="About"
-                    class={styles.anchor}
-                    href="/about"
-                /> |
-                <A
-                    children="Settings"
-                    class={styles.anchor}
-                    href="/settings"
-                /> |
+                <span>
+                    <A
+                        children="Import Vault"
+                        class={styles.anchor}
+                        href="/import-vault"
+                        onClick={handleAnchorClick}
+                    /> |
+                </span>
+                <span>
+                    <A
+                        children="About"
+                        class={styles.anchor}
+                        href="/about"
+                        onClick={handleAnchorClick}
+                    /> |
+                </span>
+                <span>
+                    <A
+                        children="Settings"
+                        class={styles.anchor}
+                        href="/settings"
+                        onClick={handleAnchorClick}
+                    /> |
+                </span>
                 <A
                     children="Donate"
                     class={styles.anchor}
                     href="/donate"
+                    onClick={handleAnchorClick}
                 />
             </nav>
         </footer>
