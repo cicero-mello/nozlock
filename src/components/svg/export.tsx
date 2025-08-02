e3911df26de530effcdba62137abe73210f76ed4
Cícero Mello <ciceromello.dev@gmail.com>
2025-08-02 14:16:25 -0300

    🍱 Adding SVG Components

---
+++ src/components/svg/export.tsx
@@ -0,0 +1,29 @@
+export const ExportSVG = () => {
+
+    return (
+        <svg
+            viewBox="0 0 32 32"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+        >
+            <path
+                class="stroke-color"
+                d="M6.625 20.75V25.125C6.625 26.5057 7.74429 27.625 9.125 27.625H22.875C24.2557 27.625 25.375 26.5057 25.375 25.125V20.75"
+                stroke="var(--color-milk-2)"
+                stroke-width="2.5"
+            />
+            <path
+                class="stroke-color"
+                d="M16 23.2501V6.71655"
+                stroke="var(--color-milk-2)"
+                stroke-width="2.5"
+            />
+            <path
+                class="stroke-color"
+                d="M21.625 11.375L16 5.75L10.375 11.375"
+                stroke="var(--color-milk-2)"
+                stroke-width="2.5"
+            />
+        </svg>
+    )
+}
---
+++ src/components/svg/import.tsx
@@ -0,0 +1,30 @@
+export const ImportSVG = () => {
+
+    return (
+        <svg
+            viewBox="0 0 32 32"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+        >
+            <path
+                class="stroke-color"
+                d="M6.625 20.75V25.125C6.625 26.5057 7.74429 27.625 9.125 27.625H22.875C24.2557 27.625 25.375 26.5057 25.375 25.125V20.75"
+                stroke="var(--color-milk-2)"
+                stroke-width="2.5"
+            />
+            <path
+                class="stroke-color"
+                d="M16 3.25L16 19.7835"
+                stroke="var(--color-milk-2)"
+                stroke-width="2.5"
+            />
+            <path
+                class="stroke-color"
+                d="M10.375 15.125L16 20.75L21.625 15.125"
+                stroke="var(--color-milk-2)"
+                stroke-width="2.5"
+            />
+        </svg>
+
+    )
+}
