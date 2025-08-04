0093567bdcb38ffc4bbddc1a0deb29c47cd50f23
Cícero Mello <ciceromello.dev@gmail.com>
2025-08-03 21:08:26 -0300

    🏗️ Simplifying Shared Button Styles (with Anchor)

--- src/components/anchor/types.ts
+++ src/components/anchor/types.ts
@@ -1,6 +1,7 @@
+import type { ButtonMode } from "../button/types"
 import type { AnchorProps as AP } from "@solidjs/router"
 
-export type AnchorMode = "main" | "soft"
+export type AnchorMode = ButtonMode
 
 export interface AnchorProps extends AP {
     mode?: AnchorMode
